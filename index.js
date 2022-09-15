5ee63e8e5bef7660d0dd2f2b0cda79c714597264
Barbara Jara <jarabarbara.m@gmail.com>
2022-09-14 21:14:19 -0300

    agrego eventos

---
+++ index.js
@@ -0,0 +1,84 @@
+/*let usuario = prompt ("igrese su usuario");
+
+if (usuario === "barbara"){
+    alert ("ingreso exitoso");
+}
+while ( usuario !== "barbara" ){
+    alert("Nombre de usuario invalido");
+    usuario = prompt ("igrese su usuario");
+}
+let edad = prompt ("Ingrese su edad")
+
+if (edad < "18") {
+    alert ("Debe ser mayor de edad para ingresar")
+    
+}else {
+    alert ("Bienvenid@ a Fructuoso Tattoo")
+}
+
+function sumaDeproductos(producto1, producto2) {
+    return producto1 + producto2;
+    
+}
+let producto1 = parseInt (prompt ("ingrese cantidad (del 1 al 10)"));
+let producto2 = parseInt (prompt ("ingrese cantidad (del 1 al 10)"));
+
+let total = sumaDeproductos(producto1, producto2) + " es el total de productos seleccionados";
+alert (total);
+
+function totalFinal (producto1, producto2) {
+    let valor = producto1 *10 + producto2 * 5;
+    return valor;
+}
+let precio = " total a pagar " + totalFinal (producto1, producto2);
+alert (precio);
+
+class Producto{
+    constructor(nombre, cantidad){
+      this.nombre = nombre;
+      this.cantidad = cantidad;
+      this.producto1 = producto1;
+     }
+  vender(){
+        if (this.producto1 >= 1) {
+            this.cantidad = this.cantidad - this.producto1;           
+        }
+    }
+    
+  }
+  class Productoj{
+    constructor(nombre, cantidad){
+      this.nombre = nombre;
+      this.cantidad = cantidad;
+      this.producto2 = producto2;
+    }
+  vender(){
+        if (this.producto2 >=1) {
+            this.cantidad = this.cantidad - this.producto2;
+        } 
+    }
+ }
+    
+  let accion1 = new Producto("Crema Postatto", 20);
+  let accion2 = new Productoj("Jabon anti-bacterial", 30);
+  console.log(accion1);
+  accion1.vender();
+  console.log(accion2);
+  accion2.vender();
+var d1 = producto1; 
+var d2 = producto2;
+  const productos = [
+    { cremas: "20" - d1},
+    { jabones: "30" - d2},
+   
+  ];
+  
+  for(const item of productos){
+    console.log(item) ;
+    
+  }
+  const obj ={ cremas: "20" - d1, jabon: "30" - d2}
+  let mensaje = `cremas en stock ${obj.cremas} jabones en stock ${obj.jabon}`;
+
+  alert(mensaje);*/
+  
---
+++ stock.js
@@ -0,0 +1,16 @@
+const productos = [
+    {
+        id:1,
+        nombre:"Crema post-tatto",
+        precio:10,
+        img: "../multimedia/imagenes/crema.jpeg",
+        cantidad:20
+    },
+    {
+        id:2,
+        nombre:"Jabón anti-bacterial",
+        precio:20,
+        img:'../multimedia/imagenes/jabon.jpeg',
+        cantidad:30
+    }
+]
